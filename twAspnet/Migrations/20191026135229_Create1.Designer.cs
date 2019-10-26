1e8b5647c652db1bb0b0dd23d56b550482549812
T.Kuzuoka <cole10133@gmail.com>
2019-10-26 23:10:32 +0900

    error sqlite?

--- twAspnet/Migrations/20191026135229_Create1.Designer.cs
+++ twAspnet/Migrations/20191026135229_Create1.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using twAspnet.Models;
 
 namespace twAspnet.Migrations
 {
     [DbContext(typeof(TwaspDbContext))]
-    partial class TwaspDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191026135229_Create1")]
+    partial class Create1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ twAspnet/Models/Enviroment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace twAspnet.Models
+{
+    public class Enviroment
+    {
+        public string Ckey { get; set; }
+        public string Csecret { get; set; }
+    }
+}

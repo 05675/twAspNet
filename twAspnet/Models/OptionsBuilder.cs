b7554487bb3e0b3769a13079c657abbe83420b13
T.Kuzuoka <cole10133@gmail.com>
2019-11-07 23:04:02 +0900

    DB is not eligible for Git and twitter API

---
+++ twAspnet/Models/OptionsBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using twAspnet.Models;
+
+namespace twAspnet.Models
+{
+    public class OptionsBuilder : DbContext
+    {
+        public OptionsBuilder(DbContextOptions<OptionsBuilder> options)
+            : base(options)
+        {
+            var option = new DbContextOptionsBuilder<TwaspDbContext>();
+            var connectionString = "Twasp.db";
+            option.UseSqlite(connectionString);
+            using var context = new TwaspDbContext(option.Options);
+            var users = context.Enviroment;
+        }
+        
+    }
+}

d9ec22b05aa222c3ab32e29b2ce92547518e902b
T.Kuzuoka <cole10133@gmail.com>
2019-11-28 22:14:57 +0900

    view error.enviroment

--- twAspnet/Controllers/EnviromentController.cs
+++ twAspnet/Controllers/EnviromentController.cs
@@ -12,39 +12,62 @@ namespace twAspnet.Controllers
 {
     public class EnviromentController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
         public IActionResult Index(IFormCollection formCollection)
         {
+
             string keyword = formCollection["search"];
 
             var option = new DbContextOptionsBuilder<TwaspDbContext>();
             var connectionString = "Twasp.db";
             option.UseSqlite(connectionString);
             using var context = new TwaspDbContext(option.Options);
-            var enviroment = context.Enviroment.Single();            
+            var enviroment = context.Enviroment.Single();
 
             if (!string.IsNullOrEmpty(keyword))
-            { 
+            {
                 var tokens = Tokens.Create(enviroment.Akey, enviroment.ASecretKey, enviroment.AToken, enviroment.ATokenSecret);
                 var result = tokens.Search.Tweets(count => 100, q => keyword);
                 //countは読み込み数。指定しなければDefoultの数値が入る。
+
+                ViewData["result"] = result;
+
                 foreach (var value in result)
                 {
                     string scrName = value.User.ScreenName; //@User_ID
                     string name = value.User.Name;          //ユーザー名
                     string text = value.Text;               //Tweet
-                                                            // textBoxStatus.AppendText("@" + scrName.ToString() + " / " + name + System.Environment.NewLine + text + System.Environment.NewLine);
+                    
+                    // textBoxStatus.AppendText("@" + scrName.ToString() + " / " + name + System.Environment.NewLine + text + System.Environment.NewLine);
                 }
             }
-                return View();
+            return View();
         }
-        
-        public void TweetSearch()
-        {
 
+        //public IActionResult Search(IFormCollection formCollection)
+        //{
+        //    string words = formCollection["words"];
+
+        //    var searchResults = new SearchResults(
+        //      Tokens.Create(
+        //        "",
+        //        "",
+        //        "",
+        //        "")
+        //        .Search.Tweets(q: words, lang: "ja", result_type: "recent", count: 100)
+        //        .ToList()
+        //        .Select(result => new SearchResult(result.Text))
+        //        .ToList());
+
+        //    //TempData.Put("searchResults", searchResults);
+        //    return RedirectToAction("ShowSearchResults", "Home");
+        //}
+
+
+
+        public IActionResult ShowSearchResults()
+        {
+            //ViewData["searchResult"] = TempData.Get<SearchResults>("searchResults");
+            return View();
         }
     }
 }

f9f385fb69bdb7529c2b17997aa236902325d531
T.Kuzuoka <cole10133@gmail.com>
2019-12-09 21:45:56 +0900

    update model favorite

--- twAspnet/Models/Favorite.cs
+++ twAspnet/Models/Favorite.cs
@@ -11,5 +11,11 @@ namespace twAspnet.Models
         public int Twid { get; set; }
         public string Comment { get; set; }
         public DateTime Favoritedate { get; set; }
+        public string Tweet { get; set; }
+        public string Name { get; set; }
+        public string ScreenName { get; set; }
+        public string UrlScreenName { get; set; }
+        public string UrlId { get; set; }
+        public string CreatedAt { get; set; }
     }
 }
